454053fe9fbbb3aed725be2aa86297ca11e310ef
schedowka <marina_molodkina@proton.me>
2024-10-15 20:14:20 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++) {
+            double funt = i * 0.453;
+            System.out.println(i + " фунтов = " + funt + " кг");
+        }
+    }
+
+
+
+
+}
